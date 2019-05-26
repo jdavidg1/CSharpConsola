22ae058422c6d0a27b1e4f77c36e60c90dd3f6dc
jdavidg1 <jdavidg_1@hotmail.com>
2019-05-25 21:01:35 -0500

    Suma por consola

--- CSharpConsola/CSharpConsola/Program.cs
+++ CSharpConsola/CSharpConsola/Program.cs
@@ -8,8 +8,23 @@ namespace CSharpConsola
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)  //El método principal se llama main, los elementos son referenciados en la parte superior
+            //El espacio de nombres es donde está ubicada una clase
         {
+            int numeroUno = int.Parse(Console.ReadLine());    //metodo parse, el cual está dentro de la clase int y por eso se llama primero
+            int numeroDos = int.Parse(Console.ReadLine());
+            int resultado = Suma(numeroUno, numeroDos);
+            Console.WriteLine($"Resultado: {resultado}");
+            Console.ReadLine();
+
+         }
+
+        static int Suma (int n1, int n2)   //Definición de método
+
+        {
+
+            return n1 + n2;
+
         }
     }
 }
